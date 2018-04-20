033ff3453a437c01adfe1217972846cbb4189458
Dali Rojas <rojasdali@gmail.com>
2018-04-20 12:09:06 -0400

    update5 fixed sound to wav

--- src/words.js
+++ src/words.js
@@ -8,8 +8,8 @@ var ctx4 = leftCanvas.getContext('2d');
 // // } else {
 // //   console.log("not found");
 // // }
-var audio = new Audio('./music/correctword.mp3');
-var audio2 = new Audio('./music/wrongword.mp3')
+var audio = new Audio('./music/correctword.wav');
+var audio2 = new Audio('./music/wrongword.wav')
 
 var points = {
     'a': 1,
